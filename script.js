04723078ed534534fa88d9f91135bb88f06a7da6
Matheus <matheus.berbet001@umb.edu>
2024-09-28 16:43:30 -0400

    final template

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 window.onload = function() {
+    // Select the specific canvas element
+    let canvas = document.getElementById('modelCanvas');
 
-    // create and initialize a 3D renderer
-    var r = new X.renderer3D();
-    r.init();
+    // Create renderer and attach to the canvas
+    let renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true , antialias: true});
+    renderer.setSize(window.innerWidth / 1.25, window.innerHeight / 2);
     
-    // create a new X.mesh
-    var porsche = new X.mesh();
-    // .. and associate the .stl to it
-    porsche.file = 'models/dna.stl';
-    // activate the magic mode which results in a colorful rendering since the
-    // point colors are based on the point position
-    porsche.magicmode = true;
-    // set a caption which appears on mouseover
-    porsche.caption = 'The magic Porsche!';
+    // Create scene, camera, and lighting
+    let scene = new THREE.Scene();
+    let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
     
-    // .. add the porsche
-    r.add(porsche);
+    let light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1, 1, 1).normalize();
+    scene.add(light);
     
-    // .. and start the loading/rendering
-    r.render();
+    // Orbit Controls
+    let controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enabled = false
+    // Load STL Model
+    let loader = new THREE.STLLoader();
+
+    loader.load('models/dna.stl', function (geometry) {
+        let material = new THREE.MeshNormalMaterial(); // Material for the mesh
+        mesh = new THREE.Mesh(geometry, material);
+        mesh.rotation.z = 1.5;
+        mesh.position.x = 6; 
+        console.log(mesh);
+        scene.add(mesh);
+    });
+    
+    // Animation loop
+    function animate() {
+        requestAnimationFrame(animate);
+        
+        
+        time = Date.now() * 0.001;
+        mesh.rotation.x = time;
+
+        controls.update();
+        renderer.render(scene, camera);
+    }
+    animate();
+    
+    // Adjust the canvas size and camera on window resize
+    window.addEventListener('resize', function () {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    });
     
 };
-  
