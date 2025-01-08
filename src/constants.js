a7284c843ad33e2e4cf91561378f7fb170f7e0a6
Matheus <matheus.berbet001@umb.edu>
2025-01-08 17:01:02 -0500

    updating paths

--- src/constants.js
+++ src/constants.js
@@ -6,28 +6,28 @@ const module1 = {
     card_title: "Module 1: A Deeper Dive in DNA Translation",
     card_image: "src/public/module1/table.png",
     card_description: "Protein translation is the process by which mRNA`s genetic code is used to build a specific chain of amino acids. It is the second major step of the central dogma...",
-    card_button: "INTRD280/blog/module1.html",
+    card_button: "blog/module1.html",
 }
 
 const module2 = {
     card_title: "Module 2: RNA-Seq Data Analysis Workflow",
     card_image: "src/public/module2/geneplot.png",
     card_description: "This page documents the steps and commands for processing RNA-Seq data. Each step is explained and the command is provided for reproducibility.",
-    card_button: "INTRD280/blog/module2.html",
+    card_button: "blog/module2.html",
 }
 
 const final = {
     card_title: "Final Project: RNA-Seq Data Analysis",
     card_image: "src/public/final/img3.png",
     card_description: "This analysis of RNA-Seq data from Salicornia aims to identify genes most influenced by nitrogen sources, utilizing transcript data, salmon pipelines, and R.",
-    card_button: "INTRD280/blog/final.html"
+    card_button: "blog/final.html"
 }
 
 const presentation = {
     card_title: "Final Presentation",
     card_image: "src/public/presentation.png",
     card_description: "This presentation provides an overview of the RNA-Seq data analysis project, including the background, methods, results, and conclusions.",
-    card_button: "INTRD280/blog/presentation.pptx"
+    card_button: "blog/presentation.pptx"
 
 }
 
